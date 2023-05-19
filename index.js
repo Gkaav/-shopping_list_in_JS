687dd7933640c432d92ffe43fd15e2200d4009cb
gigi kavelashvili <130923896+Gkaav@users.noreply.github.com>
2023-05-19 20:11:45 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
 const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button')
+
 
 function addItem(e) {
     e.preventDefault()
+
     const newItem = itemInput.value
 
     if (newItem === '') {
-        alert('Please enter something')
+        alert('Please enter a new item')
     } else {
         const li = document.createElement('li')
         li.textContent = newItem
-    
-        const button = addBtn('remove-item btn-link text-red')
-    
-        li.appendChild(button)
-    
+
         itemList.appendChild(li)
-    
+
+        const button = createbutton('remove-item btn-link text-red')
+        li.appendChild(button)
+
         newItem.value = ''
     }
 }
 
-function addBtn(classes) {
+
+function createbutton(classes) {
     const button = document.createElement('button')
     button.className = classes
-    const icon = CreateIcon('fa-solid fa-xmark')
+    const icon = createicon('fa-solid fa-xmark')
     button.appendChild(icon)
     return button
 }
 
-function CreateIcon(classes) {
+
+function createicon(classes) {
     const icon = document.createElement('i')
     icon.className = classes
     return icon
 }
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove()
+    }
+}
+
+ 
+function clearItems() {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
 
 itemForm.addEventListener('submit', addItem)
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItems)
